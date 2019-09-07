e7fe6fd34edd4fe890a1982889fa26b8c34268c4
Andrii Pavliuk <v051187paa@users.noreply.github.com>
2019-09-07 15:04:02 +0300

    add test

--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
-const express = require('express');
-require('./db/mongoose');
-const userRouter = require('./routers/user');
-const taskRouter = require('./routers/task');
+const app = require('./app');
 
-const app = express();
 const port = process.env.PORT;
-
-// app.use((req, res, next) => {
-// 	res.status(503).send('Site is under reconstruction');
-// });
-
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
 
 app.listen(port, () => {
 	console.log(`Server runs on ${port}...`);
 });
 
-const Task = require('./models/task');
-const User = require('./models/user');
+// const Task = require('./models/task');
+// const User = require('./models/user');
 
 // const main = async () => {
 // 	// const task = await Task.findById('5d6acf04dc9ac03c6cd5a9c9');
---
+++ tests/user.test.js
@@ -0,0 +1,124 @@
+const request = require('supertest');
+const app = require('../src/app');
+const User = require('../src/models/user');
+const { userOneId, userOne, setupDatabase } = require('./fixtures/db');
+
+beforeEach(setupDatabase);
+
+test('Should signup a new user', async () => {
+	const response = await request(app)
+		.post('/users')
+		.send({
+			name: 'Andy',
+			email: 'lv051187paa@gmail.com',
+			password: 'mypass111!'
+		})
+		.expect(201);
+
+	// Assert that the database was changed correctly
+	const user = await User.findById(response.body.user._id);
+	expect(user).not.toBeNull();
+
+	// Assertions about the response
+	expect(response.body).toMatchObject({
+		user: {
+			name: 'Andy',
+			email: 'lv051187paa@gmail.com'
+		},
+		token: user.tokens[0].token
+	});
+
+	expect(user.password).not.toBe('mypass111');
+});
+
+test('Should login existing user', async () => {
+	const response = await request(app)
+		.post('/users/login')
+		.send({
+			email: userOne.email,
+			password: userOne.password
+		})
+		.expect(200);
+
+	const user = await User.findById(response.body.user._id);
+	expect(response.body.token).toBe(user.tokens[1].token);
+});
+
+test('Should not login with bad creds', async () => {
+	await request(app)
+		.post('/users/login')
+		.send({
+			email: userOne.email,
+			password: 'someWrongPass'
+		})
+		.expect(400);
+});
+
+test('Should get profile for user', async () => {
+	await request(app)
+		.get('/users/me')
+		.set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+		.send()
+		.expect(200);
+});
+
+test('Should not get profile for unauthenticated user', async () => {
+	await request(app)
+		.get('/users/me')
+		.set('Authorization', `Bearer wrongToken`)
+		.send()
+		.expect(401);
+});
+
+test('Should delete account user', async () => {
+	const response = await request(app)
+		.delete('/users/me')
+		.set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+		.send()
+		.expect(200);
+
+	const user = await User.findById(userOneId);
+	expect(user).toBeNull();
+});
+
+test('Should not delete account for unauthenticated user', async () => {
+	await request(app)
+		.delete('/users/me')
+		.set('Authorization', `Bearer wrongToken`)
+		.send()
+		.expect(401);
+});
+
+test('Should upload avatar image', async () => {
+	await request(app)
+		.post('/users/me/avatar')
+		.set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+		.attach('avatar', 'tests/fixtures/profile-pic.jpg')
+		.expect(200);
+
+	const user = await User.findById(userOneId);
+	expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test('Should update valid user field', async () => {
+	const response = await request(app)
+		.patch('/users/me')
+		.set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+		.send({
+			name: 'Changed name'
+		})
+		.expect(200);
+
+	const user = await User.findById(userOneId);
+	expect(user.name).toBe('Changed name');
+});
+
+test('Should not update invalid user fields', async () => {
+	await request(app)
+		.patch('/users/me')
+		.set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+		.send({
+			location: 'Ukraine'
+		})
+		.expect(400);
+});
